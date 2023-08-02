be7028894a7ca6921224463ffe770d40e0f80ad1
Ivan Shmelev <vin.ven@yandex.ru>
2023-08-02 10:03:17 +0300

    faster

--- Roman to Integer/solution.h
+++ Roman to Integer/solution.h
@@ -7,15 +7,15 @@
 using namespace std;
 
 class Solution {
-	unordered_map<char, int> alph { {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+	unordered_map<char, int> alph {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
 
 public:
 	int romanToInt(string s) {
 		int result = 0;
 		size_t i = 0;
-		for (; i < s.size()-1; i++)
+		for (; i < s.size(); i++)
 		{
-			if (alph[s[i+1]] > alph[s[i]])
+			if (i < s.size()-1 && alph[s[i+1]] > alph[s[i]])
 			{
 				result += (alph[s[i+1]] - alph[s[i]]);
 				i++;
@@ -23,8 +23,6 @@ public:
 			}
 			result += alph[s[i]];
 		}
-		if (alph[s[i]] <= alph[s[i-1]])
-			result += alph[s[i]];
 
 		return result;
 	}
