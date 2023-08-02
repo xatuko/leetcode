38ed7625b345f4b3f08d6dffbc99c727e6451158
Ivan Shmelev <vin.ven@yandex.ru>
2023-08-02 09:58:36 +0300

    task13 complete

---
+++ Roman to Integer/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "solution.h"
+
+int main(int,char**)
+{
+    Solution s;
+    string in = "CCCI";
+    cout << s.romanToInt(in) << endl;
+    return 0;
+}
---
+++ Roman to Integer/solution.h
@@ -0,0 +1,34 @@
+#ifndef SOLUTION_H
+#define SOLUTION_H
+
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+	unordered_map<char, int> alph { {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+
+public:
+	int romanToInt(string s) {
+		int result = 0;
+		size_t i = 0;
+		for (; i < s.size()-1; i++)
+		{
+			if (alph[s[i+1]] > alph[s[i]])
+			{
+				result += (alph[s[i+1]] - alph[s[i]]);
+				i++;
+				continue;
+			}
+			result += alph[s[i]];
+		}
+		if (alph[s[i]] <= alph[s[i-1]])
+			result += alph[s[i]];
+
+		return result;
+	}
+
+};
+
+#endif // SOLUTION_H
