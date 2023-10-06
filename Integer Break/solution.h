5adfed0ba794288ea8c03a5b4ee94926816730b5
Ivan Shmelev <vin.ven@yandex.ru>
2023-10-06 12:31:06 +0300

    complete task 343

---
+++ Integer Break/solution.h
@@ -0,0 +1,21 @@
+#ifndef SOLUTION_H
+#define SOLUTION_H
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int integerBreak(int n) {
+		if (n == 2 || n == 3) return n-1;
+		if (n <= 3) return n-1;
+		int res = 1;
+		while (n > 4)
+		{
+			res *= 3;
+			n -= 3;
+		}
+		return n*res;
+	}
+};
+#endif // SOLUTION_H
